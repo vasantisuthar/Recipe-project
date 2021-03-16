a3cf56108d1effef528d25b4e186bbb57e6b0153
vasanti <vasanti@gmail.com>
2021-03-16 06:11:17 +0530

    updated callbackurl

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ passport.serializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/recipe",
+    callbackURL: "https://rocky-lake-72095.herokuapp.com/auth/google/recipe",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
